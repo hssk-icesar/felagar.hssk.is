4b21134dc0c16fdc47f01b7697b63fc007cf4248
Gudmundur Olafsson <gummiolafs@gummiolafs.is>
2021-08-22 21:20:21 +0000

    Add CNAME file for github pages

--- .eleventy.js
+++ .eleventy.js
@@ -51,6 +51,7 @@ module.exports = function (config) {
 
     // Pass-through files
     config.addPassthroughCopy('src/robots.txt')
+    config.addPassthroughCopy('src/CNAME')
     config.addPassthroughCopy('src/site.webmanifest')
     config.addPassthroughCopy('src/assets/images')
     config.addPassthroughCopy('src/assets/fonts')
