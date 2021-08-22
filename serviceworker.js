787b202e229b40ddc9bd6fa63c19f72ea54f3433
gummiolafs <gummiolafs@users.noreply.github.com>
2021-08-22 22:45:03 +0000

    deploy: 856e80c1fd286d267b2b1b42d764e6fdf04461bd

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'cache-2021-08-22T22:42:43.500'
+const CACHE_NAME = 'cache-2021-08-22T22:44:58.033'
 const OFFLINE_PAGE_URL = '/offline/'
 const PRECACHE_RESOURCES = []
 const IGNORED_HOSTS = ['localhost']
